2e231c8ef2fbcf8052fd3b78f3c570afc7b3d134
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 10:15:26 -0300

    Creating the Doctor in domain

---
+++ webapi.health.clinic/Domains/Doctor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("Doctors")]
+    [Index(nameof(CRM), IsUnique = true)]
+    public class Doctor
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "CHAR(8)")]
+        [Required(ErrorMessage = "O CRM do médico é obrigatório")]
+        public string? CRM { get; set; }
+
+        // User Reference
+
+        [Required(ErrorMessage = "O usuário é obrigatório para o médico")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; set; }
+    }
+}
