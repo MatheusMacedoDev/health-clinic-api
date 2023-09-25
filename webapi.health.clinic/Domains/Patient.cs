615a0cf97c4ffce2dc8083ed6ec75d6b77e0eb95
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 09:56:48 -0300

    Creating the Patient in domain

---
+++ webapi.health.clinic/Domains/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("Patients")]
+    public class Patient
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        // User Reference
+
+        [Required(ErrorMessage = "O usuário é obrigatório para o paciente")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; set; }
+    }
+}
