3ff3b31736789fbe7ec9507a681951ffc472853e
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 08:56:45 -0300

    Creating the Address in domain

---
+++ webapi.health.clinic/Domains/Address.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("Address")]
+    public class Address
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "CHAR(8)")]
+        [Required(ErrorMessage = "O CEP é um item obrigatório em um endereço")]
+        [MinLength(8, ErrorMessage = "Todo CEP possui exatamente oito digitos")]
+        public string? Cep { get; set; }
+
+        [Column(TypeName = "CHAR(2)")]
+        [Required(ErrorMessage = "A UF é um item obrigatório em um endereço")]
+        [MinLength(2, ErrorMessage = "Toda UF possui exatamente dois digitos")]
+        public string? Uf { get; set; }
+        
+        [Column(TypeName = "VARCHAR(30)")]
+        [Required(ErrorMessage = "A cidade é um item obrigatório em um endereço")]
+        public string? City { get; set; }
+
+        [Column(TypeName = "VARCHAR(32)")]
+        [Required(ErrorMessage = "A cidade é um item obrigatório em um endereço")]
+        public string? Neighborhood  { get; set; }
+
+        [Column(TypeName = "VARCHAR(5)")]
+        [Required(ErrorMessage = "O número é um item obrigatório em um endereço")]
+        public string? Number { get; set; }
+
+        [Column(TypeName = "VARCHAR(30)")]
+        public string? Complement { get; set; }
+    }
+}
