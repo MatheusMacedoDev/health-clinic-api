977e264ee12b57f2678a584dcc96f79e1af4f0a9
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 10:09:34 -0300

    Creating the MedicalSpecialty in domain

---
+++ webapi.health.clinic/Domains/MedicalSpecialty.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("MedicalSpecialties")]
+    [Index(nameof(Name), IsUnique = true)]
+    public class MedicalSpecialty
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(40)")]
+        [Required(ErrorMessage = "O nome da especialidade é obrigatório")]
+        public string? Name { get; set; }
+    }
+}
