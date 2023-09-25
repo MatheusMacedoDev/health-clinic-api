c030320470be822fe1352ae56d54683e07513e1c
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 10:23:18 -0300

    Creating the ClinicDoctor in domain

---
+++ webapi.health.clinic/Domains/ClinicDoctor.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("ClinicDoctors")]
+    public class ClinicDoctor
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        // Clinic Reference
+
+        [Required(ErrorMessage = "A clínica é um item obrigatório")]
+        public Guid ClinicId { get; set; }
+
+        [ForeignKey(nameof(ClinicId))]
+        public Clinic? Clinic { get; set; }
+
+        // Doctor Reference
+
+        [Required(ErrorMessage = "O médico é um item obrigatório")]
+        public Guid DoctorId { get; set; }
+
+        [ForeignKey(nameof(DoctorId))]
+        public Doctor? Doctor { get; set; }
+    }
+}
