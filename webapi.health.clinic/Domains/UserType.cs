099b0795d06f14e3c9f5958e57873460aef7cd46
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 09:24:22 -0300

    Creating the UserType in domain

---
+++ webapi.health.clinic/Domains/UserType.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("UserTypes")]
+    [Index(nameof(TypeName), IsUnique = true)]
+    public class UserType
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(32)")]
+        [Required(ErrorMessage = "O nome do tipo de usuário é um item obrigatório")]
+        public string? TypeName { get; set; }
+    }
+}
