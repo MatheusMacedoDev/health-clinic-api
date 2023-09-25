97a615bf902d9f1165c6ee572bba1a954b9b469a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-09-25 10:20:33 -0300

    Creating the DoctorMedicalSpecialty in domain

---
+++ webapi.health.clinic/Domains/DoctorMedicalSpecialty.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("DoctorMedicalSpecialties")]
+    public class DoctorMedicalSpecialty
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        // Doctor Reference
+
+        [Required(ErrorMessage = "O médico é um campo obrigatório")]
+        public Guid DoctorId { get; set; }
+
+        [ForeignKey(nameof(DoctorId))]
+        public Doctor? Doctor { get; set; }
+
+        // Medical Specialty Reference
+
+        [Required(ErrorMessage = "A especialidade médica é um campo obrigatório")]
+        public Guid MedicalSpecialtyId { get; set; }
+
+        [ForeignKey(nameof(MedicalSpecialtyId))]
+        public MedicalSpecialty? MedicalSpecialty { get; set; }
+    }
+}
