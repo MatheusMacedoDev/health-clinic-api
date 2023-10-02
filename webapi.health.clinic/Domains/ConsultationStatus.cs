84567a64138c2e5a5adc36811afd84821456cd10
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-02 10:15:34 -0300

    Creating the ConsultationStatus domain

---
+++ webapi.health.clinic/Domains/ConsultationStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("ConsultationStatus")]
+    public class ConsultationStatus
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "O estado da consulta deve possuir um nome", AllowEmptyStrings = false)]
+        public string? StatusName { get; set; }
+    }
+}
