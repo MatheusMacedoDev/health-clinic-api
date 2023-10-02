a0aec3697840b90aff57fde6870ab973ad99f7cd
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-02 10:11:24 -0300

    Creating the MedicalRecord domain

---
+++ webapi.health.clinic/Domains/MedicalRecord.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("MedicalRecords")]
+    public class MedicalRecord
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "O prontuário deve conter alguma descrição", AllowEmptyStrings = false)]
+        public string? Text { get; set; }
+    }
+}
