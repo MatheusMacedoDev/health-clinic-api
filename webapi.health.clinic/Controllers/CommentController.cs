cc3f7f4df6be1b4f39dca0cdef44e18c27388ff6
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-02 10:55:33 -0300

    Creating the CommentController

---
+++ webapi.health.clinic/Controllers/CommentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.health.clinic.Domains;
+using webapi.health.clinic.Interfaces;
+using webapi.health.clinic.Repositories;
+
+namespace webapi.health.clinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController()
+        {
+            _commentRepository = new CommentRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Comment comment)
+        {
+            try
+            {
+                _commentRepository.Create(comment);
+
+                return StatusCode(201, comment);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpGet("{consultationId}")]
+        public IActionResult GetByConsultation(Guid consultationId)
+        {
+            try
+            {
+                List<Comment> comments = _commentRepository.GetByConsultation(consultationId);
+
+                return Ok(comments);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _commentRepository.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+    }
+}
