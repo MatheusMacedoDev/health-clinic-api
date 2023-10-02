b49d15557f5802987ae8bc5321727cc879f4e22f
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-02 10:04:50 -0300

    Creating the Comment domain

---
+++ webapi.health.clinic/Domains/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health.clinic.Domains
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "Date")]
+        public DateTime Date { get; set; } = DateTime.Now.Date;
+
+        [Column(TypeName = "VARCHAR(300)")]
+        [Required(ErrorMessage = "O comentário deve conter algum texto", AllowEmptyStrings = false)]
+        [StringLength(300, MinimumLength = 1)]
+        public string? Text { get; set; }
+
+        [Column(TypeName = "BIT")]
+        public bool IsValidated { get; set; } = false;
+
+        // Consultation Reference
+
+        [Required(ErrorMessage = "É obrigatório definir uma consulta a ser comentada")]
+        public Guid ConsultationId { get; set; }
+
+        [ForeignKey(nameof(ConsultationId))]
+        public Consultation? Consultation { get; set; }
+    }
+}
