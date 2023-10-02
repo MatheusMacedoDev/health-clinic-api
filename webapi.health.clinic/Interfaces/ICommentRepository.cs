7526f6d33339357d6ac2bb2442e1ffeb7d372582
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-10-02 10:55:07 -0300

    Creating the ICommentRepository

---
+++ webapi.health.clinic/Interfaces/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using webapi.health.clinic.Domains;
+
+namespace webapi.health.clinic.Interfaces
+{
+    public interface ICommentRepository
+    {
+        void Create(Comment comment);
+        List<Comment> GetByConsultation(Guid consultationId);
+        void Delete(Guid id);
+    }
+}
