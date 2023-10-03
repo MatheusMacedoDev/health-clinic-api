70270c9299023046f8b6bca81765e33a8f531246
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-10-03 08:17:43 -0300

    Creating the IMedicalRecordRepository

---
+++ webapi.health.clinic/Interfaces/IMedicalRecordRepository.cs
@@ -0,0 +1,11 @@
+﻿using webapi.health.clinic.Domains;
+
+namespace webapi.health.clinic.Interfaces
+{
+    public interface IMedicalRecordRepository
+    {
+        void Create(MedicalRecord medicalRecord);
+        MedicalRecord GetById(Guid id);
+        void Update(MedicalRecord medicalRecord);
+    }
+}
