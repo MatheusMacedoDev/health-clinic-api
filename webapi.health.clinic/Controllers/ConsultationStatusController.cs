1809146a0ffa6d930d4c36c7f321ff3bbfc06351
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-10-03 08:47:35 -0300

    Creating the ConsultationStatusController

---
+++ webapi.health.clinic/Controllers/ConsultationStatusController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.health.clinic.Domains;
+using webapi.health.clinic.Interfaces;
+using webapi.health.clinic.Repositories;
+
+namespace webapi.health.clinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ConsultationStatusController : ControllerBase
+    {
+        private readonly IConsultationStatusRepository _consultationStatusRepository;
+
+        public ConsultationStatusController()
+        {
+            _consultationStatusRepository = new ConsultationStatusRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ConsultationStatus consultationStatus)
+        {
+            try
+            {
+                _consultationStatusRepository.Create(consultationStatus);
+
+                return StatusCode(201, consultationStatus);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ListAll()
+        {
+            try
+            {
+                List<ConsultationStatus> consultationStatus = _consultationStatusRepository.ListAll();
+
+                return Ok(consultationStatus);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _consultationStatusRepository.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+    }
+}
