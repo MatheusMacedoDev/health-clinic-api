f30c3ae6311e20422ea1a831a9152e41f54bce7e
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-10-03 08:46:50 -0300

    Creating the IConsultationStatusRepository

---
+++ webapi.health.clinic/Interfaces/IConsultationStatusRepository.cs
@@ -0,0 +1,11 @@
+﻿using webapi.health.clinic.Domains;
+
+namespace webapi.health.clinic.Interfaces
+{
+    public interface IConsultationStatusRepository
+    {
+        void Create(ConsultationStatus consultationStatus);
+        List<ConsultationStatus> ListAll();
+        void Delete(Guid id);
+    }
+}
