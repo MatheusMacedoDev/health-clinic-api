8af773d42c8ec2bf2de99e544e135b96d3330cb8
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-10-03 08:18:09 -0300

    Creating the MedicalRecordRepository

---
+++ webapi.health.clinic/Repositories/MedicalRecordRepository.cs
@@ -0,0 +1,40 @@
+﻿using webapi.health.clinic.Contexts;
+using webapi.health.clinic.Domains;
+using webapi.health.clinic.Interfaces;
+
+namespace webapi.health.clinic.Repositories
+{
+    public class MedicalRecordRepository : IMedicalRecordRepository
+    {
+        private readonly HealthClinicContext _context;
+
+        public MedicalRecordRepository()
+        {
+            _context = new HealthClinicContext();
+        }
+
+        public void Create(MedicalRecord medicalRecord)
+        {
+            _context.MedicalRecords.Add(medicalRecord);
+            _context.SaveChanges();
+        }
+
+        public MedicalRecord GetById(Guid id)
+        {
+            return _context.MedicalRecords.FirstOrDefault(record => record.Id == id)!;
+        }
+
+        public void Update(MedicalRecord medicalRecord)
+        {
+            MedicalRecord findedMedicalRecord = GetById(medicalRecord.Id);
+
+            if (findedMedicalRecord != null)
+            {
+                findedMedicalRecord.Text = medicalRecord.Text;
+
+                _context.MedicalRecords.Update(findedMedicalRecord);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
